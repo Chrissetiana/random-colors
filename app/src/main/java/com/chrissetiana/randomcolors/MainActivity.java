e66a2d08463e6c1d44bcdcf3162c533efde60116
Tiana Aiyemo <chrissetiana@gmail.com>
2019-02-27 00:37:09 +0100

    Add: colors

--- app/src/main/java/com/chrissetiana/randomcolors/MainActivity.java
+++ app/src/main/java/com/chrissetiana/randomcolors/MainActivity.java
@@ -3,13 +3,22 @@ package com.chrissetiana.randomcolors;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private String[] colorArray = {"red", "pink", "purple", "deep_purple",
+            "indigo", "blue", "light_blue", "cyan", "teal", "green",
+            "light_green", "lime", "yellow", "amber", "orange", "deep_orange",
+            "brown", "grey", "blue_grey", "black"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
     }
 
     public void onChange(View view) {
